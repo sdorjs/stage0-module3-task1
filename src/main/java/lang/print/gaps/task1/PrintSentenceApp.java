89d7182f04ace4d1a1c3aad63b07a07a20020925
sdorjs <128179615+sdorjs@users.noreply.github.com>
2023-03-17 22:02:37 +0300

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -1,6 +1,6 @@
 package lang.print.gaps.task1;
 
-public class BrokenApp {
+public class PrintSentenceApp {
     public static void main(String[] args) {
         System.out.println("This is my first java program\n");
         System.out.println("This is my first Java program\n");
